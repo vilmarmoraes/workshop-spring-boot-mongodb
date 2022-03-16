cf8bff1fece1946dd48d879975fa413ab074f1ff
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-03-15 22:34:50 -0300

    Obtendo um usuário por id

--- src/main/java/com/vilmarmoraes/workshopmongo/services/UserService.java
+++ src/main/java/com/vilmarmoraes/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.vilmarmoraes.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vilmarmoraes.workshopmongo.domain.User;
 import com.vilmarmoraes.workshopmongo.repository.UserRepository;
+import com.vilmarmoraes.workshopmongo.services.exception.ObjectNotFoundException;
 
 
 	@Service
@@ -19,11 +21,12 @@ import com.vilmarmoraes.workshopmongo.repository.UserRepository;
 			return repo.findAll();
 		}
 
-		/*public User findById(String id) {
+		public User findById(String id) {
 			Optional<User> obj = repo.findById(id);
-			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nÃ£o encontrado"));
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
 
+/*
 		public User insert(User obj) {
 			return repo.insert(obj);
 		}
