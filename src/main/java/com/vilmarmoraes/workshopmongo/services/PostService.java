9e4710b60f96277a0181d7f8aa0f074c81027d4f
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-03-22 22:04:47 -0300

    Obtendo um post por id

---
+++ src/main/java/com/vilmarmoraes/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.vilmarmoraes.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vilmarmoraes.workshopmongo.domain.Post;
+import com.vilmarmoraes.workshopmongo.repository.PostRepository;
+import com.vilmarmoraes.workshopmongo.services.exception.ObjectNotFoundException;
+
+
+	@Service
+	public class PostService {
+
+		@Autowired
+		private PostRepository repo;
+
+
+
+		public Post findById(String id) {
+			Optional<Post> obj = repo.findById(id);
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+}
